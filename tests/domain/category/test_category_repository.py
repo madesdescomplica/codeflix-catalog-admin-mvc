135b40da19a3d598323eed8dee3627af812beff2
mades eladrian <mades.eladrian@gmail.com>
2024-06-15 23:39:33 -0300

    ensure CategoryRepository raises a NotImplementedError if save method is not implemented

--- tests/domain/category/test_category_repository.py
+++ tests/domain/category/test_category_repository.py
@@ -1,8 +1,38 @@
 from inspect import isabstract
+from unittest.mock import patch
+from uuid import uuid4
 
-from src.domain.category import CategoryRepository
+from faker import Faker
+import pytest
+
+from src.domain.category import Category, CategoryRepository
 
 
 class TestCategoryRepository:
+    faker = Faker()
+
+    @pytest.fixture
+    def category(self) -> Category:
+        return Category(
+            id=uuid4(),
+            name=self.faker.word(),
+            description=self.faker.sentence(),
+            is_active=True
+        )
+
+    @pytest.fixture
+    @patch.multiple(CategoryRepository, __abstractmethods__=set())
+    def category_repository(self) -> CategoryRepository:
+        return CategoryRepository()
+
+
     def test_should_CategoryRepository_is_an_abstract_class(self):
         assert isabstract(CategoryRepository)
+
+    def test_should_CategoryRepository_raise_a_NotImplementedError_if_save_method_is_not_implemented(
+        self,
+        category_repository: CategoryRepository,
+        category: Category
+    ):
+        with pytest.raises(NotImplementedError, match='Should implement method: save'):
+            category_repository.save(category)
