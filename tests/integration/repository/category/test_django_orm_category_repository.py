5a0f988464903e0ee6796984928fca68cb6009c4
mades eladrian <mades.eladrian@gmail.com>
2024-06-19 22:19:43 -0300

    ensure DjangoORMCategoryRepository list categories in database

--- tests/integration/repository/category/test_django_orm_category_repository.py
+++ tests/integration/repository/category/test_django_orm_category_repository.py
@@ -19,6 +19,14 @@ class TestDjangoORMCategoryRepository:
         )
 
     @pytest.fixture
+    def other_category(self):
+        return Category(
+            name=self.faker.word(),
+            description=self.faker.sentence(),
+            is_active=self.faker.boolean()
+        )
+
+    @pytest.fixture
     def repository(self):
         return DjangoORMCategoryRepository()
 
@@ -51,3 +59,17 @@ class TestDjangoORMCategoryRepository:
         assert category_db.name == category.name
         assert category_db.description == category.description
         assert category_db.is_active == category.is_active
+
+    def test_list_categories_in_database(
+        self,
+        category: Category,
+        other_category: Category,
+        repository: DjangoORMCategoryRepository
+    ):
+        assert CategoryModel.objects.count() == 0
+        repository.save(category)
+        repository.save(other_category)
+        assert CategoryModel.objects.count() == 2
+
+        category_db = repository.list()
+        assert category_db == [category, other_category]
